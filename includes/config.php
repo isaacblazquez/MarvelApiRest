92ce447d6155a7fd0ed692a058b20f47b96adcaf
Isaac Blazquez <isaac.blazquez@gmail.com>
2023-05-16 00:40:21 +0200

    config.php

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,9 @@
 <?php
-//public key
-$pk="a614ad95f9c7037cbd6f52f3d84978c4";
-//private key 
-$pr_k="dea2435911dc24a0fd827e7cdac2465707ec6a96";
+// you must fill it with your public API key
+$pk="";
+// you must fill it with your private API key
+$pr_k="";
+
 $key=$pr_k.$pk;
 $currentTime = time();
 $hash=md5($currentTime.$key);
